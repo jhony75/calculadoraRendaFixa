47ced209ef4cac10ea3443876578b55c06e536a5
jhony75 <jonatas.lima.medeiros@live.com>
2024-03-09 22:49:34 -0300

    initial commit

---
+++ lib/calculadoraRendaFixa.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "calculadoraRendaFixa/version"
+
+module CalculadoraRendaFixa
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/calculadoraRendaFixa_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CalculadoraRendaFixa do
+  it "has a version number" do
+    expect(CalculadoraRendaFixa::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
