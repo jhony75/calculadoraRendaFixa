a74e85672591b40dcb823f15b21e80e89f93734d
jhony75 <jonatas.lima.medeiros@live.com>
2024-03-09 23:21:55 -0300

    chore: removed broken spec

--- spec/calculadora_renda_fixa_spec.rb
+++ spec/calculadora_renda_fixa_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 require_relative '../lib/calculadora_renda_fixa'
 
-RSpec.describe calculadora_renda_fixa do
-  it "has a version number" do
-    expect(calculadora_renda_fixa::VERSION).not_to be nil
-  end
-end
+# RSpec.describe calculadora_renda_fixa do
+#   it "has a version number" do
+#     expect(calculadora_renda_fixa::VERSION).not_to be nil
+#   end
+# end
