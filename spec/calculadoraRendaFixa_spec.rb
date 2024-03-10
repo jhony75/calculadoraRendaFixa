62dab322004c2f8e5f3ed5588d4c36d00c4ed743
jhony75 <jonatas.lima.medeiros@live.com>
2024-03-09 22:59:03 -0300

    chore: updated spec to test pipeline

--- spec/calculadoraRendaFixa_spec.rb
+++ spec/calculadoraRendaFixa_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CalculadoraRendaFixa do
   it "has a version number" do
     expect(CalculadoraRendaFixa::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
